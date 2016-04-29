9ad4b44c4e3a35172edf4b241f72a5b20c6624f5
Juan Sepulveda <juan@lavaina.com>
2016-04-29 01:04:59 -0400

    Create jsep:jquery.tokeninput

---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  name: 'jsep:jquery.tokeninput',
+  version: '1.6.2_1',
+  // Brief, one-line summary of the package.
+  summary: 'A Tokenizing Autocomplete Text Entry',
+  git: 'https://github.com/jsep/jquery-tokeninput',
+  documentation: 'README.md'
+});
+
+Npm.depends({
+  "jquery": "2.2.3"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3');
+  api.use('ecmascript');
+
+  api.mainModule('src/jquery.tokeninput.js', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use("jsep:jquery.tokeninput");
+  api.use("ecmascript");
+  api.use("templating");
+  api.use("practicalmeteor:mocha");
+
+  api.addFiles("tests/test.html", "client");
+  api.addFiles("tests/exists.js", "client");
+});
